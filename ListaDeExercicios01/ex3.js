7f745b9f9e8ea421a7493b7bd7eb6d2b0e7ae04b
Your Name <you@example.com>
2024-04-04 20:15:23 -0300

    exercicio

--- ListaDeExercicios01/ex3.js
+++ ListaDeExercicios01/ex3.js
@@ -6,7 +6,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+});  
 
 rl.question('Digite o salário mensal atual do funcionário: ', (salarioAtual) => {
     rl.question('Digite o percentual de reajuste: ', (percentualReajuste) => {
